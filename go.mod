b744cee571297d8cf95b07bd1b151ae2bcba44de
yasudanaoya <43776161+yasudanaoya@users.noreply.github.com>
2021-05-26 18:06:08 +0900

    feat: add go-prompt

--- go.mod
+++ go.mod
@@ -1,3 +1,12 @@
 module github.com/yasudanaoya/shain_go_cli
 
 go 1.14
+
+require (
+	github.com/c-bata/go-prompt v0.2.6
+	github.com/mattn/go-colorable v0.1.8 // indirect
+	github.com/mattn/go-runewidth v0.0.13 // indirect
+	github.com/nasum/culc v0.0.0-20181216121539-fb9798f9a636
+	github.com/spf13/cobra v1.1.3
+	golang.org/x/sys v0.0.0-20210525143221-35b2ab0089ea // indirect
+)
--- main.go
+++ main.go
@@ -1,14 +1,24 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
+
+	"github.com/c-bata/go-prompt"
 )
 
+func completer(d prompt.Document) []prompt.Suggest {
+	s := []prompt.Suggest{
+		{Text: "1", Description: "社員を登録"},
+		{Text: "2", Description: "社員を参照"},
+		{Text: "3", Description: "社員を更新"},
+		{Text: "4", Description: "社員を削除"},
+		{Text: "5", Description: "終わる"},
+	}
+	return prompt.FilterHasPrefix(s, d.GetWordBeforeCursor(), true)
+}
+
 func main() {
-	fmt.Print("input? ")
-	scanner := bufio.NewScanner(os.Stdin)
-	scanner.Scan()
-	fmt.Println("input is", scanner.Text())
+	fmt.Println("メニュー選択してね")
+	t := prompt.Input("> ", completer)
+	fmt.Println("You selected " + t)
 }
